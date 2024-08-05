e8438fea237243696746065df11d0363bd0703e6
Nikhil-Mohan-01 <127566613+Nikhil-Mohan-01@users.noreply.github.com>
2024-08-04 22:46:05 -0400

    Facial Rec

--- FacialRecognition/facialrec.py
+++ FacialRecognition/facialrec.py
@@ -1,3 +1,54 @@
 import threading
-
 import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+
+reference_img_path = r"C:\Users\nikhi\OneDrive\Documents\Github\Pose-Estimation-Autonomous-Turret\FacialRecognition\Nikhil.jpg"
+reference_img = cv2.imread(reference_img_path)
+
+# Check if the image was loaded correctly
+if reference_img is None:
+    print(f"Error: Could not load the image at path {reference_img_path}")
+    exit()
+
+face_match = False
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+    except Exception as e:
+        print(f"Error in face verification: {e}")
+        face_match = False
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+        if face_match:
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow('video', frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
